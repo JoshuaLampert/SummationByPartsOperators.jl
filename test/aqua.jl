35689114027800096aaad79d0d0803d4967e46a1
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-03-20 11:38:21 +0100

    allow PreallocationTools as stale dep for Aqua.jl

--- test/aqua.jl
+++ test/aqua.jl
@@ -4,4 +4,4 @@ using SummationByPartsOperators
 Aqua.test_all(SummationByPartsOperators;
               ambiguities = false, # a lot of false positives from dependencies
               unbound_args = false, # TODO: a strange problem I do not understand right now
-              stale_deps = (; ignore = [:PrecompileTools, :InteractiveUtils]),)
+              stale_deps = (; ignore = [:PrecompileTools, :InteractiveUtils, :PreallocationTools]),)
